0d08002bb5ca2cdc9f9e77c00ef7c8574e4716f5
Nuby <nuby.4register@gmail.com>
2024-02-27 20:20:03 +0300

    77

---
+++ src/prefillAnArray.js
@@ -0,0 +1,20 @@
+function isValidInteger(n) {
+  if (
+    typeof n === 'boolean' ||
+    !isFinite(n) ||
+    !Number.isInteger(Number(n)) ||
+    Number(n) < 0
+  )
+    throw new TypeError(`${n} is invalid`)
+}
+
+// Long Solution
+function prefill(n, v) {
+  isValidInteger(n)
+
+  const array = []
+  array.length = n
+  array.fill(v)
+
+  return array
+}
